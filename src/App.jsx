b3f4d4ce8dd9f342cce6f9c4091c65ec9eb560e4
luis-luciqm <redbullfc18@gmail.com>
2023-03-11 11:01:35 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import { Post } from './Posts'
+
+export function App() {  
+  return (
+    <>
+      <Post/>
+    </>
+  )
+}
+
+export default App
